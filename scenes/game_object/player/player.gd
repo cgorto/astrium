676adb658799120ac92c78b2a51c369f12412e84
cgorto <christian.gorton@colorado.edu>
2024-09-27 09:50:49 -0600

    init

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -9,3 +9,7 @@ func _physics_process(delta: float) -> void:
 	var mvmt_dir: Vector2 = movement_component.get_movement()
 	velocity_component.accelerate_in_direction(mvmt_dir)
 	velocity_component.move(self)
+	
+	
+	
+	
