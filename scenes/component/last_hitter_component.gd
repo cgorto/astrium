b6fddf7479ff78eb9a66f976d2ccf8f6e29b59c8
cgorto <christian.gorton@colorado.edu>
2024-09-30 10:49:41 -0600

    last hit stuff

---
+++ scenes/component/last_hitter_component.gd
@@ -0,0 +1,41 @@
+class_name LastHitterComponent extends Node2D
+
+@export var cooldown_time: float = 5.0
+@onready var cooldown_timer: Timer = $MissCooldown
+@onready var line: Line2D = $Line2D
+@onready var visible_timer: Timer = $Line2D/VisibleTimer
+
+var target_global_position: Vector2 = Vector2.ZERO
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if not cooldown_timer.is_stopped():
+			return
+		var space_state:PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
+		var query: = PhysicsPointQueryParameters2D.new()
+		query.position = get_global_mouse_position()
+		
+		target_global_position = query.position
+		line.visible = true
+		visible_timer.start()
+		
+		
+		query.collision_mask = 8
+		query.collide_with_areas = true
+		var result: = space_state.intersect_point(query)
+		
+		if result:
+			for collision in result:
+				var collider: Area2D = collision.collider
+				if collider is LastHitOrb:
+					collider.last_hit()
+
+func _process(delta: float) -> void:
+	if line.visible:
+		var local_position: Vector2 = to_local(target_global_position)
+		line.points = [Vector2.ZERO, local_position]
+
+
+func _on_visible_timer_timeout() -> void:
+	line.visible = false
+	
--- scenes/game_object/last_hit_orb/last_hit_orb.gd
+++ scenes/game_object/last_hit_orb/last_hit_orb.gd
@@ -4,9 +4,10 @@ signal orb_popped(popped: LastHitOrb)
 
 @export var initial_value: int = 1
 @export var max_value: int = 5
-@export var lifetime: float = 3
-@export var initial_speed: float = 100
-@export var friction: float = 50
+var lifetime: float = 3
+var initial_speed: float = 100
+var friction: float = 50
+
 
 var current_value: int
 var time_alive: float = 0
@@ -19,15 +20,19 @@ var time_alive: float = 0
 func _ready() -> void:
 	current_value = initial_value
 	lifetime = randf_range(2,4)
-	initial_speed = randf_range(50,100)
-	friction = randf_range(25,75)
+	initial_speed = randf_range(150,250)
+	friction = randf_range(5,15)
 	velocity_component.velocity = Vector2.RIGHT.rotated(randf()*2*PI)*initial_speed
 
 func _physics_process(delta: float) -> void:
-	position += velocity_component.velocity *delta
+	velocity_component.move(self)
 	velocity_component.decelerate()
 	
 	time_alive += delta
 	
 	if time_alive >= lifetime:
 		health_component.damage(1)
+		
+func last_hit() -> void:
+	print("last hit")
+	health_component.damage(1)
