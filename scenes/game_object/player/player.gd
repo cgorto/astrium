ae0d9b8655540b403acec7fd99588d14aea2ab35
cgorto <christian.gorton@colorado.edu>
2024-10-08 13:12:09 -0600

    added shader

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -5,15 +5,18 @@ extends CharacterBody2D
 @onready var movement_component: MovementComponent = $MovementComponent
 @onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
 @onready var rotation_component: RotationComponent = $RotationComponent
+@onready var animation_tree: AnimationTree = $AnimationTree
 
 
 func _physics_process(delta: float) -> void:
 	var mvmt_dir: Vector2 = movement_component.get_movement()
 	velocity_component.accelerate_in_direction(mvmt_dir)
 	if mvmt_dir != Vector2.ZERO:
-		rotation_component.rotate_towards_direction(mvmt_dir)
+		$RayCast2D.target_position = mvmt_dir * 100
+		animation_tree["parameters/blend_position"] = mvmt_dir
+		#rotation_component.rotate_towards_direction(mvmt_dir)
 	velocity_component.move(self)
-	
+
 
 
 
